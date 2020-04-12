86e5a93a715efff35dde1cba97f5a73a42d2828f
Adrijan <adrijan9@gmail.com>
2020-04-12 18:06:12 +0200

    removed line

--- src/axiosRepository.js
+++ src/axiosRepository.js
@@ -102,11 +102,10 @@ const service = {
     searchTodo: (searchTerm) => {
         return axios.get(`/todo/search?term=${searchTerm}`);
 
-    }
     },
     getReviews: () => {
         return axios.get("http://all.local/_Projects/Freelancing/angrus/api/reviews.php");
-    },
+    }
 };
 
 export default service;
